1234078e0e274f3fa4988ad39895d5f052409dd9
Mayank Kumar <mayankkumar82488@gmail.com>
2025-08-03 13:16:48 +0530

    Added socket.io in the backend and wrote the corresponding code

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,14 +1,46 @@
-// import { log } from "console";
-import express, { Request, Response } from "express";
-
-const app = express();
+import express from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
+import cors from "cors";
+import { off } from "process";
 const port = 4000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("App Working");
-})
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"]
+    }
+});
 
-})
+io.on("connection", (socket: Socket) => {
+    console.log("Client connected", socket.id);
+
+    socket.on("offer", (offer: RTCSessionDescriptionInit) => {
+        console.log("Offer recieved");
+        socket.broadcast.emit("offer", offer);
+    });
+
+    socket.on("answer", (answer: RTCSessionDescriptionInit) => {
+        console.log("Answer recieved");
+        socket.broadcast.emit("answer", answer);
+    });
+
+    socket.on("ice-candidate", (candidate: RTCSessionDescriptionInit) => {
+        console.log("Ice candidate recieved");
+        socket.broadcast.emit("ice-candidate", candidate);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    })
+
+});
+
+server.listen(port, () => {
+    console.log("Server running");
+
+});
