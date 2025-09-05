602c818eac004513b12af2e4d52193d27ef6ab2a
Mayank Kumar <mayankkumar82488@gmail.com>
2025-09-05 19:38:45 +0530

    Established p2p connection

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,45 +1,70 @@
 import express from "express";
-import http from "http";
+import https from "https";
+import fs from "fs";
 import { Server, Socket } from "socket.io";
 import cors from "cors";
-const port = 4000;
 
+const port = 4000;
 const app = express();
 app.use(cors());
-const server = http.createServer(app);
+
+const privateKey = fs.readFileSync("./certificates/localhost+2-key.pem", "utf8");
+const certificate = fs.readFileSync("./certificates/localhost+2.pem", "utf8");
+
+
+const server = https.createServer(
+    { key: privateKey, cert: certificate },
+    app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:4200",
+        // origin: ["http://localhost:4500", "http://192.168.129.222:4500"],
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
 
+let waitingSocket: Socket | null = null;
+
 io.on("connection", (socket: Socket) => {
     console.log("Client connected", socket.id);
 
-    socket.on("offer", (offer: RTCSessionDescriptionInit) => {
-        console.log("Offer recieved");
-        socket.broadcast.emit("offer", offer);
+    socket.on("start-call", () => {
+        if (waitingSocket?.id === socket.id) {
+            console.log("User tried to match with themselves—ignoring.");
+            return;
+        }
+        if (!waitingSocket) {
+            waitingSocket = socket;
+            socket.emit("waiting");
+        } else {
+            // Match found
+            socket.emit("matched", { peerId: waitingSocket.id });
+            waitingSocket.emit("matched", { peerId: socket.id });
+            waitingSocket = null;
+        }
     });
 
-    socket.on("answer", (answer: RTCSessionDescriptionInit) => {
-        console.log("Answer recieved");
-        socket.broadcast.emit("answer", answer);
+    socket.on("offer", ({ offer, to }) => {
+        io.to(to).emit("offer", { offer, from: socket.id });
     });
 
-    socket.on("ice-candidate", (candidate: RTCSessionDescriptionInit) => {
-        console.log("Ice candidate recieved");
-        socket.broadcast.emit("ice-candidate", candidate);
+    socket.on("answer", ({ answer, to }) => {
+        io.to(to).emit("answer", { answer, from: socket.id });
+    });
+
+    socket.on("ice-candidate", ({ candidate, to }) => {
+        io.to(to).emit("ice-candidate", { candidate, from: socket.id });
     });
 
     socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    })
-
+        console.log("Client disconnected", socket.id);
+        if (waitingSocket?.id === socket.id) {
+            waitingSocket = null;
+        }
+    });
 });
 
 server.listen(port, () => {
     console.log("Server running");
-
 });
